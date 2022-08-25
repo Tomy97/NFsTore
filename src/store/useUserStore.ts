9d25598d8b329ac65059dabbc0a9a5548d96c8a6
Tomas <tomas.pandu@gmail.com>
2022-08-24 21:50:24 -0300

    cambio de los stores, vuecore y cambios

---
+++ src/store/useUserStore.ts
@@ -0,0 +1,14 @@
+import { defineStore } from "pinia";
+
+export const useUserStore = defineStore({
+  id: "user",
+  state: () => ({
+    userData: null,
+  }),
+  getters: {
+    isAuth: (state: any) => {},
+  },
+  actions: {
+    async login({ commit }: any, userData: any) {},
+  },
+});
