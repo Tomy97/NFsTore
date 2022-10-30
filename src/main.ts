abb1f39baf43bac7f209243c2f4aeebf89da95fc
Tomas <tomas.pandu@gmail.com>
2022-10-30 04:21:44 -0300

    validaciones en espa;ol

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia';
-import './style.css'
-import App from './App.vue'
-import routes from './routes/index'
-
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import "./style.css";
+import App from "./App.vue";
+import routes from "./routes/index";
+import '../src/setup/veeValidate'
 
 const pinia = createPinia();
 createApp(App).use(routes).use(pinia).mount("#app");
