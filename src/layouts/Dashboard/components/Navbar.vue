33b5ed131925a3537f8d8c89afe6212665aa5e35
Tomas Pandullo <64372041+Tomy97@users.noreply.github.com>
2023-11-04 02:13:49 -0300

    .

--- src/layouts/Dashboard/components/Navbar.vue
+++ src/layouts/Dashboard/components/Navbar.vue
@@ -26,9 +26,11 @@ const menuLinks = [
   },
 ];
 const isMobileMenuOpen = ref<boolean>(false);
-
 const authStore = useAuthStore();
-const toggleNavMenu = () => {
+
+const isOpenMenu = ref<boolean>(false);
+const openSideMenu = (val: boolean) => {};
+const toggleResponsiveNavMenu = () => {
   isMobileMenuOpen.value = !isMobileMenuOpen.value;
 };
 </script>
@@ -41,13 +43,13 @@ const toggleNavMenu = () => {
         </router-link>
       </div>
       <div class="lg:hidden">
-        <button class="navbar-toggler" type="button" @click="toggleNavMenu">
+        <button class="navbar-toggler" type="button" @click="toggleResponsiveNavMenu">
           <i class="bi bi-list"></i>
         </button>
       </div>
-      <div class="hidden lg:flex space-x-2">
+      <div class="hidden lg:flex space-x-2 items-center">
         <!-- Enlaces del menú -->
-        <template v-for="(item, index) of menuLinks" :key="index">
+        <template v-for="item of menuLinks" :key="item.id">
           <router-link
             :to="{ name: item.name }"
             class="block text-black capitalize"
@@ -56,13 +58,16 @@ const toggleNavMenu = () => {
           </router-link>
         </template>
         <div v-if="authStore.isAuth" class="space-x-2">
-          <span>
-            {{ authStore.user?.userName }}
-          </span>
-          <button>
-
-          </button>
-          <BtnLogout @click="authStore.logOut" />
+          <div
+            class="flex -space-x-2 overflow-hidden cursor-pointer"
+            @click="openSideMenu((isOpenMenu = !isOpenMenu))"
+          >
+            <img
+              class="inline-block h-10 w-10 rounded-full ring-2 ring-white"
+              :src="authStore.user?.avatar"
+              :alt="authStore.user?.name"
+            />
+          </div>
         </div>
         <div v-else>
           <BtnDinamic :path="{ name: 'Login' }" text="Login" />
@@ -73,7 +78,7 @@ const toggleNavMenu = () => {
       <div v-if="isMobileMenuOpen" class="md:hidden">
         <div class="bg-white p-4">
           <div class="space-y-2">
-            <template v-for="(item, index) of menuLinks" :key="index">
+            <template v-for="item of menuLinks" :key="item.id">
               <router-link
                 :to="{ name: item.name }"
                 class="block text-black capitalize"
@@ -84,7 +89,13 @@ const toggleNavMenu = () => {
           </div>
           <div class="mt-4 text-center">
             <div v-if="authStore.isAuth" class="space-x-2">
-              <!-- Botones de autenticación -->
+              <div class="flex -space-x-2 overflow-hidden">
+                <img
+                  class="inline-block h-10 w-10 rounded-full ring-2 ring-white"
+                  src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+              </div>
             </div>
             <div v-else>
               <BtnDinamic :path="{ name: 'Login' }" text="Login" />
